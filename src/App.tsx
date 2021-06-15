de051fc1659b478a8f9ca52bf3c6ffd7400d37e3
Kirill <vladimirov.kirilllll@gmail.com>
2021-06-15 16:41:55 +0300

    added Map in App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Map from "react-map-gl";
+
+const TOKEN =
+  "pk.eyJ1Ijoia2lyaWxsbGx2IiwiYSI6ImNrcHh1NXIzZzBlb2cyd253d2t2bmo1bWEifQ.1y30G02TkPr-TRYHLCpHDA";
+
+interface viewport {
+  latitude: number;
+  longitude: number;
+  width: string;
+  height: string;
+  zoom: number;
+}
 
 function App() {
+  const [viewport, setViewport] = React.useState<viewport>({
+    latitude: 53.902334,
+    longitude: 27.5618791,
+    width: "100vw",
+    height: "100vh",
+    zoom: 13,
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        {...viewport}
+        mapboxApiAccessToken={TOKEN}
+        onViewportChange={(viewport: any) => setViewport(viewport)}></Map>
     </div>
   );
 }
