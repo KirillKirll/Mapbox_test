18d48a4bcb3cd00111d4fafc46398b54b1c29c16
Kirill <vladimirov.kirilllll@gmail.com>
2021-06-15 20:32:08 +0300

    added test markers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Map from "react-map-gl";
+import Map, { Marker } from "react-map-gl";
 import Button from "@material-ui/core/Button";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 
@@ -15,6 +15,11 @@ interface viewport {
   zoom: number;
 }
 
+interface MarkerTypes {
+  latitude: any;
+  longitude: any;
+}
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -25,16 +30,39 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-function App() {
+const App: React.FC = () => {
   const classes = useStyles();
-
   const [viewport, setViewport] = React.useState<viewport>({
     latitude: 53.902334,
     longitude: 27.5618791,
     width: "100vw",
     height: "100vh",
-    zoom: 13,
+    zoom: 11,
   });
+  const [marker, setMarker] = React.useState<MarkerTypes[]>([]);
+  console.log(marker);
+
+  const handleClick = () => {
+    for (let i = 0; i < 40; i++) {
+      const getRandomCoordinate = (min: number, max: number) => {
+        return Math.random() * (max - min) + min;
+      };
+
+      const dots = {
+        latitude: getRandomCoordinate(53.857974, 53.942472),
+        longitude: getRandomCoordinate(27.48243, 27.570224),
+      };
+
+      setMarker((state) => {
+        return [...state, dots];
+      });
+    }
+
+    // latitude:
+    // getRandomLatitude(54.000472, 53.794074),
+    // // longitude:
+    // getRandomLongitude(27.31243, 27.788224),
+  };
 
   return (
     <div className="App">
@@ -43,16 +71,23 @@ function App() {
         mapboxApiAccessToken={TOKEN}
         onViewportChange={(viewport: any) => setViewport(viewport)}>
         <div className={classes.root}>
-          <Button variant="contained" color="primary">
+          <Button variant="contained" color="primary" onClick={handleClick}>
             markers
           </Button>
           <Button variant="contained" color="primary">
             split
           </Button>
         </div>
+        {marker.map((element, id) => {
+          return (
+            <Marker latitude={element.latitude} longitude={element.longitude} key={id}>
+              HELLO
+            </Marker>
+          );
+        })}
       </Map>
     </div>
   );
-}
+};
 
 export default App;
